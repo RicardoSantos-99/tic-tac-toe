42f9dfb30bfcaca35272dbc466038a6477ab5d85
RicardoSantos-99 <ricardo2010carvalho@hotmail.com>
2023-04-02 19:58:27 -0300

    feat: user crud

---
+++ lib/tic_tac_toe_web/controllers/user_html.ex
@@ -0,0 +1,13 @@
+defmodule TicTacToeWeb.UserHTML do
+  use TicTacToeWeb, :html
+
+  embed_templates "user_html/*"
+
+  @doc """
+  Renders a user form.
+  """
+  attr :changeset, Ecto.Changeset, required: true
+  attr :action, :string, required: true
+
+  def user_form(assigns)
+end
